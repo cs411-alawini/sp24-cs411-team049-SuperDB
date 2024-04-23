9a6ef4d221a9b1b03e312f03bc38de0cdff0870d
Kathy <admin@MacBook-Pro-5.lan1>
2024-04-23 14:20:56 -0700

    Change to IDENTITY generation type for userID

--- server/src/main/java/com/housing/entity/UserEntity.java
+++ server/src/main/java/com/housing/entity/UserEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class UserEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userID;
 
     @Column(nullable = false, length = 50)
