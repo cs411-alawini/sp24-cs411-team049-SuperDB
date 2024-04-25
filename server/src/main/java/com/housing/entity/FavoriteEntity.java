2039901f5450069ca1414bd0c6bfb404e6ffd864
terry626 <terrytian626@gmail.com>
2024-04-25 09:31:59 -0500

    add-interface：favorite

--- server/src/main/java/com/housing/entity/FavoriteEntity.java
+++ server/src/main/java/com/housing/entity/FavoriteEntity.java
@@ -15,15 +15,13 @@ public class FavoriteEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int favoriteID;
 
-    @Column(nullable = false)
     private int userID;
 
-    @Column(nullable = false)
+    private Long propertyID;
+
     private int listingID;
 
-    @Column(nullable = false, precision = 10)
     private BigDecimal priceAtFavTime;
 
-    @Column(nullable = false)
     private Date favTime;
 }
