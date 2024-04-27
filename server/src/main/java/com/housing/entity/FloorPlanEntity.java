d1534c99bd9d1ff05990886ef1f276d18d9bbd31
terry626 <terrytian626@gmail.com>
2024-04-26 20:05:19 -0500

    add-interface：update-property

--- server/src/main/java/com/housing/entity/FloorPlanEntity.java
+++ server/src/main/java/com/housing/entity/FloorPlanEntity.java
@@ -11,7 +11,7 @@ public class FloorPlanEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int floorPlanID;
+    private Integer floorPlanID;
 
     @Column(nullable = false)
     private Long propertyID;
