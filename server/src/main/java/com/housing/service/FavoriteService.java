16c7288cb1fbb939011a94caca6463df5eb520ad
terry626 <terrytian626@gmail.com>
2024-04-26 22:26:20 -0500

    favorite

--- server/src/main/java/com/housing/service/FavoriteService.java
+++ server/src/main/java/com/housing/service/FavoriteService.java
@@ -1,5 +1,9 @@
 package com.housing.service;
 
+import java.util.List;
+
 public interface FavoriteService {
     void addFavorite(int userId, Long propertyId);
+    void removeFavorite(int userId, Long propertyId);
+    List<Long> getFavorites(int userId);
 }
