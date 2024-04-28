c4b4dafcd4e24ff738867750cc1c342aaec9eab6
terry626 <terrytian626@gmail.com>
2024-04-28 18:52:38 -0500

    contactnumber

--- server/src/main/java/com/housing/controller/PropertyController.java
+++ server/src/main/java/com/housing/controller/PropertyController.java
@@ -4,6 +4,7 @@ import com.housing.entity.PropertyModel;
 import com.housing.service.PropertyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,16 +29,28 @@ public class PropertyController {
 
     @PostMapping("/properties/create")
     public ResponseEntity<?> createProperty(@RequestBody PropertyModel property) {
-        log.info("Creating new property with details: {}", property);
-        propertyService.createProperty(property);
-        return ResponseEntity.ok("OK");
+        try {
+            log.info("Creating new property with details: {}", property);
+            propertyService.createProperty(property);
+            return ResponseEntity.ok("OK");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while creating the property");
+        }
     }
 
     @PutMapping("/properties/update/")
     public ResponseEntity<?> updateProperty(@RequestBody PropertyModel property) {
-        log.info("Updating property with details: {}", property);
-        propertyService.updateProperty(property);
-        return ResponseEntity.ok("OK");
+        try {
+            log.info("Updating property with details: {}", property);
+            propertyService.updateProperty(property);
+            return ResponseEntity.ok("OK");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while updating the property");
+        }
     }
 
     @DeleteMapping("/properties/delete/{propertyId}")
