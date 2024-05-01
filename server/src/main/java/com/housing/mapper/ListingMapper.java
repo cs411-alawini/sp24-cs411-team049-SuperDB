c73559399a124597805ef06b7f681480ace7cabb
terry626 <terrytian626@gmail.com>
2024-04-30 22:55:27 -0500

    listing

--- server/src/main/java/com/housing/mapper/ListingMapper.java
+++ server/src/main/java/com/housing/mapper/ListingMapper.java
@@ -1,11 +1,11 @@
 package com.housing.mapper;
 
-import com.housing.entity.ListingModel;
+import com.housing.entity.ListingEntity;
 import org.apache.ibatis.annotations.Mapper;
-import java.util.List;
 
 @Mapper
 public interface ListingMapper {
 
-    List<ListingModel> findAvailableListingsWithHighRatings();
+    void insertListing(ListingEntity listing);
+
 }
